484fe3be4f49dde586cd359d1cc4dd09019c34cc
Kaickpl <kaick.mlopes@upe.br>
2026-05-14 21:05:36 -0300

    Ajustes de erros

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:espectrum_front/Config/tema_claro.dart';
 import 'package:espectrum_front/Config/tema_escuro.dart';
-import 'package:espectrum_front/View/Pages/home_aluno.dart';
-import 'package:espectrum_front/View/Pages/pagina_inicial.dart';
 import 'package:espectrum_front/View/Pages/pagina_protocolo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
