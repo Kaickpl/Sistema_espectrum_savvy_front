4bb2bdfcc2ea1add9952adbe9851a811a9869520
Gabi <gabimelo640@gmail.com>
2026-06-25 21:59:12 -0300

    Correção das cores na página home de responsavel e professor

---
+++ lib/Config/coresPadrao.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract final class CoresPadrao {
+  static const Color naoComecadoCor = Color.fromARGB(255, 156, 39, 16);
+  static const Color emProgressoCor = const Color.fromARGB(255, 254, 197, 62);
+  static const Color concluidoCor = Color.fromARGB(255, 1, 102, 9);
+}
